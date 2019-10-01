406eec72ab13691e80b5cc314da0bb46b06b0a34
khangnkn <khangnkn@vng.com.vn>
2019-10-01 14:01:05 +0700

    proj: add docker-compose

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var axios = require('axios')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get("http://authentication:3000/auth")
+  .then(resp => {
+    console.log(resp)
+    res.send("xxx" + resp.data)
+    return
+  })
+  .catch(error => {
+    console.log(error)
+    res.send(error)
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
