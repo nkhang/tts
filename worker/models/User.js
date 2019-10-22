cf7fc4257a4c6f47e23488d2bd62ef3b5ed025ac
nguyenthanhnhut5897 <nguyennhut581997@gmail.com>
2019-10-22 23:46:36 +0700

    add token and error code

--- worker/models/User.js
+++ worker/models/User.js
@@ -26,6 +26,10 @@ const UserSchema = new Schema({
   updateAt: {
     type: Date,
     default: Date.now
+  },
+  token: {
+    type: String,
+    required: true
   }
 });
 
--- worker/validation/register.js
+++ worker/validation/register.js
@@ -9,36 +9,44 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : '';
   data.numberPhone = !isEmpty(data.numberPhone) ? data.numberPhone : '';
 
-  if (!Validator.isLength(data.fullname, { min: 2, max: 50 })) {
-    errors.fullname = 'fullname must be between 2 and 50 characters';
-  }
-
-  if (Validator.isEmpty(data.fullname)) {
-    errors.fullname = 'fullname field is required';
-  }
-
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
-
-  if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
-  }
-
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
-  }
-
-  if (!Validator.isLength(data.password, { min: 8, max: 30 })) {
-    errors.password = 'Password must be at least 8 characters';
-  }
-
   if (Validator.isEmpty(data.numberPhone)) {
-    errors.numberPhone = 'NumberPhone field is required';
+    errors.message = 'NumberPhone field is required';
+    errors.code = 1006
   }
 
   if (!Validator.isMobilePhone(data.numberPhone)) {
-    errors.numberPhone = "NumberPhone is invalid"
+    errors.message = "NumberPhone is invalid"
+    errors.code = 1007
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.message = 'Password field is required';
+    errors.code = 1004
+  }
+
+  if (!Validator.isLength(data.password, { min: 8, max: 30 })) {
+    errors.message = 'Password must be at least 8 characters';
+    errors.code = 1005
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.message = 'Email field is required';
+    errors.code = 1002
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.message = 'Email is invalid';
+    errors.code = 1001
+  }
+
+  if (!Validator.isLength(data.fullname, { min: 2, max: 50 })) {
+    errors.message = 'fullname must be between 2 and 50 characters';
+    errors.code = 1008
+  }
+
+  if (Validator.isEmpty(data.fullname)) {
+    errors.message = 'fullname field is required';
+    errors.code = 1009
   }
 
   return {
