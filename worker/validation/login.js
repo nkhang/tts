bc5cdb4c6c064df63e636818c80bbbc23363aca7
nguyenthanhnhut5897 <nguyennhut581997@gmail.com>
2019-10-22 23:46:36 +0700

    add token and error code

--- worker/validation/login.js
+++ worker/validation/login.js
@@ -7,16 +7,19 @@ module.exports = function validateLoginInput(data) {
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
 
+  if (Validator.isEmpty(data.password)) {
+    errors.message = 'Password field is required';
+    errors.code = 1003
+  }
+
   if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+    errors.message = 'Email is invalid';
+    errors.code = 1001
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
-
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+    errors.message = 'Email field is required';
+    errors.code = 1002
   }
 
   return {
