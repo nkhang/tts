c41ae490abf750abe445003855c551ee74f291f5
khangnkn <khangnkn@vng.com.vn>
2019-10-31 19:38:38 +0700

    project: deploy friendly

--- web/app.js
+++ web/app.js
@@ -1,30 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const passport = require("passport");
+require("./middlewares/passport-local");
+const BodyParser = require("body-parser");
+const listEndpoints = require("express-list-endpoints");
+var vwRoute = require("./routes/views/index");
+var apiRoute = require("./routes/apis/index");
+const cors = require("cors");
+const userParser = require("./middlewares/parse.mdw");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// register middleware
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(passport.initialize());
+app.use(userParser);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// register routes
+app.use("/", vwRoute);
+app.use("/api", apiRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,11 +43,13 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
+console.log(listEndpoints(app));
 
 module.exports = app;
--- web/configs/authConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = WorkerPath= () => {
-    let scheme = 'http'
-    let host = global.config.worker.host
-    let port = global.config.worker.port
-    if (global.config.worker.ssl) {
-      scheme = 'https'
-    } 
-    return `${scheme}://${host}:${port}`
-  }
