5b7da770c27e60b72bafba0b5b9fe9758b9eb07a
nguyenthanhnhut5897 <nguyennhut581997@gmail.com>
2019-11-04 19:36:07 +0700

    add api tts

--- server_tts/server.py
+++ server_tts/server.py
@@ -5,6 +5,7 @@ from flask_jsonpify import jsonify
 from werkzeug.utils import secure_filename
 import os
 from gtts import gTTS
+from docx import Document
 
 UPLOAD_FOLDER = 'uploads\\'
 OUTPUT_FOLDER = 'outputs\\'
@@ -24,31 +25,57 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def textToSpeech(content, filenameOutput, language, prefixFilename):
-    output = gTTS(text = content, lang = language, slow = False)
+def getDocText(filename):
+    doc = Document(filename)
+    fullText = ""
+    for para in doc.paragraphs:
+        fullText += para.text
+    return fullText
+	
+def textToSpeech(filenameInput, filenameOutput, language, prefixFilename):
+    encoding = 'ansi'
+    if language == "vi":
+        encoding = 'utf8'
+
+    myText = ''
+    if prefixFilename == 'txt':
+        with open(filenameInput, "r",  encoding = encoding) as fh:
+            myText = fh.read().replace("\n", " ")
+        fh.close()
+    elif prefixFilename == 'docx':
+        myText = getDocText(filenameInput).replace('\n',' ')
+    
+    output = gTTS(text = myText, lang = language, slow = False)
     output.save(filenameOutput)
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
-	result = request.get_json()
-	ffilename = result['filename']
-	language = result['language']
-	content = result['content']
-
-	if not ffilename:
+	if 'file' not in request.files:
 		errors = {'data': {}, 'error': {'message': 'invalid pagram', 'code': 300}}
 		return jsonify(errors)
+	f = request.files['file']
+	
+	if 'language' not in request.form:
+		errors = {'data': {}, 'error': {'message': 'invalid pagram', 'code': 300}}
+		return jsonify(errors)
+	language = request.form['language']
 
-	if allowed_file(ffilename):
-		perfix = ffilename.rsplit('.', 1)[1].lower()
-		filename = ffilename.rsplit('.', 1)[0]
-		filenameInput = ffilename
+	if f.filename == '':
+		errors = {'data': {}, 'error': {'message': 'no file', 'code': 301}}
+		return jsonify(errors)
+	
+	if f and allowed_file(f.filename):
+		f.save(secure_filename(f.filename))
+		
+		perfix = f.filename.rsplit('.', 1)[1].lower()
+		filename = f.filename.rsplit('.', 1)[0]
+		filenameInput = f.filename
 		filenameOutput = OUTPUT_FOLDER + filename + '.mp3'
 		
 		if not os.path.exists(OUTPUT_FOLDER):
 			os.makedirs(OUTPUT_FOLDER)
 			
-		textToSpeech(content, filenameOutput, language, perfix)
+		textToSpeech(filenameInput, filenameOutput, language, perfix)
 		
 		if os.path.exists(filenameInput):
 			os.remove(filenameInput)
