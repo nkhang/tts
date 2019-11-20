24762314baf76cb7c826fbc9eb275b382700165a
Nguyen Khac Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2019-11-20 23:08:43 +0700

    web: backend for profile

--- web/routes/views/index.js
+++ web/routes/views/index.js
@@ -5,8 +5,10 @@ var stepRoutes = require('./step')
 const authmdw = require('../../middlewares/auth.mdw');
 
 router.use('/', publicRoutes)
-router.get("/profile", (req, res, next) => {
-    res.render("profile");
+router.use('/step', authmdw, stepRoutes)
+
+router.get("/profile", authmdw, (req, res, next) => {
+    res.render("profile", {user: res.locals.user});
   });
 
 module.exports = router;
