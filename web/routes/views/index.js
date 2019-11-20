88c5ef4bbda89538fcea87eb49a6406d20a6ba6b
Nguyen Khac Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2019-11-20 22:47:51 +0700

    web: authen before process step

--- web/routes/views/index.js
+++ web/routes/views/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var publicRoutes = require('./public');
+var stepRoutes = require('./step')
+const authmdw = require('../../middlewares/auth.mdw');
 
 router.use('/', publicRoutes)
-
+router.use('/step', authmdw, stepRoutes)
 module.exports = router;
